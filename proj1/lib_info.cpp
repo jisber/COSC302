0aa743652df564b7dd64e26b9a059adbe7bcf31d
Jacob Isber <jisber@hydra15.eecs.utk.edu>
2020-01-21 20:50:30 -0500

    Adding To Project

--- proj1/lib_info.cpp
+++ proj1/lib_info.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "user.hpp"
 
 using namespace std;
 
@@ -12,17 +13,19 @@ int main(int argc, char* argv[]) {
 	string line;
 	ifstream fin;
 
+	temp.title = "hello";
+	cout << temp.title << endl;
+	
+	//Opens and checks the file.
 	fin.open(argv[1]);
-
 	if(fin.fail()) cout << "Could not open file" << endl;
 
-	while(getline(fin, line)) {
+	while(getline(fin, line)) { //Reads in each line from the text file.
+	
+		for(int i = 0; i < line.size(); i++) if(line[i] == '_') line[i] = ' ';	//Converts all "_" to a " " in a text file.
 		
-		
-
-			
 	}
-
+	
 
 
 	return 0;
---
+++ proj1/user.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <set>
+#include <map>
+
+using namespace std;
+
+struct Song { 
+	string title;
+	int time;  // could also be a string
+	int track;
+};
+
+struct Album {
+	map <int, Song > songs;
+	string name;
+	int time;
+	int nsongs;  // optional variable but makes it easier
+};
+
+struct Artist {
+	map <string, Album > albums;
+	string name;
+	int time;
+	int nsongs;
+};
