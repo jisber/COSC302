7e4b2067329b3b37e0d9c1a72c916e397ae857cc
Dani Shekoury <dshekou1@hydra7.eecs.utk.edu>
2020-01-22 00:20:59 -0500

    update def.cpp

--- proj1/def.cpp
+++ proj1/def.cpp
@@ -1,8 +1,23 @@
+#include <iostream>
 #include <set>
 #include <map>
+#include <string>
+#include <sstream>
 #include "user.hpp"
 using namespace std;
 
-void GetString(string str, set<Artist> Artset){
+void GetString(string str){
+//	Artist tArtist;
+//	Album tAlbum;
+//	Song tSong;
+
+	stringstream ss(str);
+
+	do{
+		string word;
+		ss >> word;
+
+		cout << word << endl;
+	} while(ss);
 
 }
